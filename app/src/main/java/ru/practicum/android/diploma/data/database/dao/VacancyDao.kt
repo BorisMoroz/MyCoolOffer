773d9c9334a395639d10b2dbb5bd61e39c905221
Dan Mikheenko <miheenko.denis@gmail.com>
2025-02-15 14:15:25 +0200

    fixed: added functions to dao

--- app/src/main/java/ru/practicum/android/diploma/data/database/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/database/dao/VacancyDao.kt
@@ -1,7 +1,20 @@
 package ru.practicum.android.diploma.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.data.database.entity.VacancyEntity
 
 @Dao
 interface VacancyDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertVacancy(vacancy: VacancyEntity)
+
+    @Query("SELECT * FROM vacancies_table ORDER BY publishedAt DESC")
+    fun getAllVacancies(): Flow<List<VacancyEntity>>
+
+    @Query("SELECT * FROM vacancies_table WHERE id = :vacancyId")
+    fun getVacancyById(vacancyId: String): VacancyEntity?
 }
