ff6825f1d23e439569862c9537e666af8ac901a7
Dan Mikheenko <miheenko.denis@gmail.com>
2025-02-15 13:30:40 +0200

    fixed: added base structure

---
+++ app/src/main/java/ru/practicum/android/diploma/data/database/AppDatabase.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    // TODO: необходимо добавить в  в DI в DataModule инициализацию БД
+}
