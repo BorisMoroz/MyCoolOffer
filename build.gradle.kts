27cf19d5f52fdfc3497ae427eeea85d62bb022ec
Danil <wastoyou@mail.ru>
2025-02-15 12:38:42 +0300

    feat: добавил основные либы, обновил другие + добавил настройки в Manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("convention.detekt")
+    id("androidx.navigation.safeargs.kotlin") version "2.8.7" apply false
 }
