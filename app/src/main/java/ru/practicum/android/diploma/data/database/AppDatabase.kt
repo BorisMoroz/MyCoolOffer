b577f30874bfe6ceeecb93c8969ac60476d71c94
Dan Mikheenko <miheenko.denis@gmail.com>
2025-02-15 14:03:55 +0200

    fixed: added dao and entity

--- app/src/main/java/ru/practicum/android/diploma/data/database/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/database/AppDatabase.kt
@@ -2,7 +2,9 @@ package ru.practicum.android.diploma.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ru.practicum.android.diploma.data.database.dao.VacancyDao
 
 @Database(version = 1)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun vacancyDao(): VacancyDao
 }
