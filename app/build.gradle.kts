28e03033ca04f53de12d1d7dd6bf5e4c155cbe64
Dan Mikheenko <miheenko.denis@gmail.com>
2025-02-16 21:35:58 +0200

    Использую ksp вместо kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("ru.practicum.android.diploma.plugins.developproperties")
     id("androidx.navigation.safeargs.kotlin")
     id("kotlin-parcelize")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp") version libs.versions.ksp.get()
 }
 
 android {
@@ -52,9 +52,9 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.androidX.core)
     implementation(libs.androidX.appCompat)
-    implementation("androidx.room:room-runtime:2.5.1")
-    kapt("androidx.room:room-compiler:2.5.1")
-    implementation("androidx.room:room-ktx:2.5.1")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
     // UI layer libraries
     implementation(libs.ui.material)
