a71e3883ae72df84e560a31558ef987a26410dbd
BorisMoroz <154249644+BorisMoroz@users.noreply.github.com>
2025-02-17 23:02:07 +0300

    Заготовка для сетевых запросов (Retrofit)

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HHApi.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.data.network
+
+const val HHBASEURL = "https://api.hh.ru/"
+
+interface HHApi
--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,5 +1,38 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.network.HHApi
+import ru.practicum.android.diploma.data.network.HHBASEURL
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
-val dataModule = module {}
+private const val MYCOOLOFFER_PREFERENCES = "mycooloffer_preferences"
+
+val dataModule = module {
+    single<HHApi> {
+        Retrofit.Builder()
+            .baseUrl(HHBASEURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HHApi::class.java)
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(get(), get())
+    }
+
+    single { Gson() }
+
+    single {
+        androidContext().getSystemService(Context.CONNECTIVITY_SERVICE)
+    }
+
+    single {
+        androidContext().getSharedPreferences(MYCOOLOFFER_PREFERENCES, Context.MODE_PRIVATE)
+    }
+}
