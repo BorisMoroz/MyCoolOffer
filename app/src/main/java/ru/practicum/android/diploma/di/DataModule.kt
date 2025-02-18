716ebd8672fd808bc7fd41f06127033cc3c29887
AZhDevelop <azh.develop@mail.ru>
2025-02-18 22:34:01 +0300

    Epic 0 check commit

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,28 +1,23 @@
 package ru.practicum.android.diploma.di
 
 import android.content.Context
+import androidx.room.Room
 import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.database.AppDatabase
 import ru.practicum.android.diploma.data.network.HHApi
 import ru.practicum.android.diploma.data.network.HHBASEURL
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
-import androidx.room.Room
-import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module
-import ru.practicum.android.diploma.data.database.AppDatabase
 
 private const val MYCOOLOFFER_PREFERENCES = "mycooloffer_preferences"
 
 val dataModule = module {
     single<HHApi> {
-        Retrofit.Builder()
-            .baseUrl(HHBASEURL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        Retrofit.Builder().baseUrl(HHBASEURL).addConverterFactory(GsonConverterFactory.create()).build()
             .create(HHApi::class.java)
     }
 
@@ -39,10 +34,9 @@ val dataModule = module {
     single {
         androidContext().getSharedPreferences(MYCOOLOFFER_PREFERENCES, Context.MODE_PRIVATE)
     }
-        
+
     single {
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
-            .fallbackToDestructiveMigration()
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db").fallbackToDestructiveMigration()
             .build()
     }
 }
