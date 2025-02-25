ecb176e5850374b7bc7e305f358952f5dfe1bb5f
Dan Mikheenko <miheenko.denis@gmail.com>
2025-02-25 20:14:22 +0200

    Удален ненужный импорт

--- app/src/main/java/ru/practicum/android/diploma/ui/search/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/VacancyViewHolder.kt
@@ -8,7 +8,6 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.models.Vacancy
 import java.text.NumberFormat
