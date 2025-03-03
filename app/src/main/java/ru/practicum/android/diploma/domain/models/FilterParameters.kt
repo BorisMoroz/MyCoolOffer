f6eadb1f1a5c2861b1af5fcf0dd00b74e5226128
AZhDevelop <azh.develop@mail.ru>
2025-03-04 00:06:33 +0300

    Все значения в FilterParameters заменены на val

--- app/src/main/java/ru/practicum/android/diploma/domain/models/FilterParameters.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/FilterParameters.kt
@@ -1,12 +1,12 @@
 package ru.practicum.android.diploma.domain.models
 
 data class FilterParameters(
-    var countryId: String,
-    var countryName: String,
-    var areaId: String,
-    var areaName: String,
-    var industryId: String,
-    var industryName: String,
-    var salary: Int,
-    var onlyWithSalary: Boolean
+    val countryId: String,
+    val countryName: String,
+    val areaId: String,
+    val areaName: String,
+    val industryId: String,
+    val industryName: String,
+    val salary: Int,
+    val onlyWithSalary: Boolean
 )
